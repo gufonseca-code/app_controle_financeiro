2f157bdd08562f6a6d155982f02d75c87c836929
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2025-04-15 23:16:48 -0300

    Implement expense tracking functionality with input handling and delete option

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const input =  document.getElementById("todo-input")
+const input =  document.getElementById("expenses-input")
 const addButton =  document.getElementById("add-button")
 const expensesList = document.getElementById("expenses-list")
 
 addButton.addEventListener("click", () => {
     if (input.value.trim() === "") return;
-    
-})
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+    ${input.value}
+    <button class = "delete-button">Delete</button>
+    `;
+
+    expensesList.appendChild(li);
+    input.value = "";
+
+    li.querySelector(".delete-button").addEventListener("click", () => {
+        li.remove();
+    });
+});
+
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") addButton.click();
+});
