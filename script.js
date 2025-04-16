886acc018628d9595904add08b52466a5254a147
Gustavo Oliveira <gufonseca.oliveira@gmail.com>
2025-04-15 22:45:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const input =  document.getElementById("todo-input")
+const addButton =  document.getElementById("add-button")
+const expensesList = document.getElementById("expenses-list")
+
+addButton.addEventListener("click", () => {
+    if (input.value.trim() === "") return;
+    
+})
